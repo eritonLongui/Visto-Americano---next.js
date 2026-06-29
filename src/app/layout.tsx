01968a1e4d0428273594418a086ee735eb122b9f
Ériton Santos | Nostrali <7eriton@gmail.com>
2026-06-29 09:09:50 -0300

    Adicionando openGraph preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Cairo, DM_Serif_Display } from 'next/font/google';
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://visto-americano-next-js.vercel.app/"),
   title: "Visto Americano | Nostrali",
   description: "Acesso reservado e seletivo para os vistos americanos EB-1 e EB-2 NIW. Realize sua análise prévia de elegibilidade de forma segura e confidencial com nosso corpo técnico e jurídico.",
   keywords: ["Visto Americano", "EB-1", "EB-2 NIW", "Green Card", "Nostrali Cidadania Italiana", "Imigração EUA", "Assessoria de Visto", "Qualificação de Visto"],
@@ -12,6 +13,14 @@ export const metadata: Metadata = {
     description: "Formulário de qualificação de possibilidade de vistos nas categorias EB-1 e EB-2 NIW.",
     type: "website",
     locale: "pt_BR",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Vistos Americanos EB-1 e EB-2 NIW",
+      },
+    ],
   }
 };
 
